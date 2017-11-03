d8197d948b3a7232d1e7505ec4162021d85eafba
Andrew <milldog8@hotmail.com>
2017-11-03 11:37:11 -0600

    finished

--- ramp-up/Controller/RampPickerVC.swift
+++ ramp-up/Controller/RampPickerVC.swift
@@ -34,6 +34,8 @@ class RampPickerVC: UIViewController {
         
         preferredContentSize = size
         
+        view.layer.borderColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        view.layer.borderWidth = 3.0
         let scene = SCNScene(named: "art.scnassests/ramps.scn")!
         sceneView.scene = scene
         
@@ -64,6 +66,7 @@ class RampPickerVC: UIViewController {
             let node = hitResults[0].node
             print(node.name!)
             rampPlacerVC.onRampSelected(node.name!)
+            dismiss(animated: true, completion: nil)
         }
     }
 
